6d90bab910055e874917405ef7b9f0bc26f8f376
Bowgo Tsai <bowgotsai@google.com>
2019-04-05 09:53:14 +0000

    davinci: AVB: Enable AVB
    Change-Id: I7f8071fdd5792cfd5814baed9846f040f2c1f9fc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,7 +199,8 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_cepheus
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
