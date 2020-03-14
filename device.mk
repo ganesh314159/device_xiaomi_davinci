8a8e34222d0e17b9a72fd0a319183a749e26f66e
Lucas Dupin <dupin@google.com>
2020-03-14 14:44:40 -0700

    davinci: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I3352b34739f5f45efa4088044de0e030449ff866

--- device.mk
+++ device.mk
@@ -174,6 +174,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.davinci
