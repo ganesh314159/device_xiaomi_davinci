382530dc913c22a05c78fb6ec38edc3d144a7a69
Lucas Dupin <dupin@google.com>
2020-03-23 10:55:10 -0700

    davinci: Default blurs to on on coral and flame
    Bug: 149792636
    Test: make
    Change-Id: Id78b72bd3925db21c4c963298c3684e3b54ba43a

--- device.mk
+++ device.mk
@@ -174,10 +174,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
-# Enable blurs, hidden under dev option
+# Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
 # Fingerprint
