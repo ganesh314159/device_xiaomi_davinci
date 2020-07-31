a509c336feb3e2d59c4a301b83a5c8cd6cdead09
trpsl <trpsl755@gmail.com>
2020-07-31 11:33:05 +0200

    Revert "cepheus: Build mac-generator"
    This reverts commit 637474e50bc95d23608638e96993b22644fb620a.

--- device.mk
+++ device.mk
@@ -442,10 +442,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
-# WiFi
-PRODUCT_PACKAGES += \
-    wifi-mac-generator
-
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
