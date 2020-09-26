c738d4f20e3ae619f1ac9f0e17b00c914028af4b
trpsl <trpsl755@gmail.com>
2020-09-27 01:28:19 +0200

    davinci: build thermal.sm6150

--- device.mk
+++ device.mk
@@ -423,7 +423,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
-    thermal.msmnile
+    thermal.sm6150
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
