c50ada0d9770de1cdf199c034a67e62823932b34
trpsl <trpsl755@gmail.com>
2020-09-27 02:33:28 +0200

    davinci: update bluetooth blobs

--- device.mk
+++ device.mk
@@ -122,12 +122,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
     BluetoothQti \
-    libbthost_if \
-    libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    libldacBT_dec
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
