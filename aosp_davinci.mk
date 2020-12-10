c14096fb09d2d198f8cecb029fee75deef31f330
trpsl <trpsl@pixelexperience.org>
2020-12-10 11:02:09 +0100

    davinci: build aosp recovery
    Change-Id: I789a71f34e4db48caa26b781b8cb909c5bf58ed7

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_USES_AOSP_RECOVERY := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_davinci
