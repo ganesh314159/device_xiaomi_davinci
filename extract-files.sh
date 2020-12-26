b18f92124333b54e0dc72d5f03cb50cb24727a75
trpsl <trpsl@pixelexperience.org>
2020-12-26 17:30:06 +0100

    davinci: sync extracting scripts
    Change-Id: Iee186c921c8b0deb5bdd5853531f942c55846c51

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2019 The Paranoid Android Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The PixelExperience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,35 +24,39 @@ VENDOR=xiaomi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ROOT="$MY_DIR"/../../..
+AOSP_ROOT="${MY_DIR}/../../.."
 
-HELPER="$ROOT"/vendor/lucid/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
+SECTION=
+KANG=
 
 while [ "$1" != "" ]; do
-    case $1 in
+    case "$1" in
         -n | --no-cleanup )     CLEAN_VENDOR=false
-        ;;
+                                ;;
+        -k | --kang)            KANG="--kang"
+                                ;;
         -s | --section )        shift
-        SECTION=$1
-        CLEAN_VENDOR=false
-        ;;
-        * )                     SRC=$1
-        ;;
+                                SECTION="$1"
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC="$1"
+                                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
+if [ -z "${SRC}" ]; then
     SRC=adb
 fi
 
@@ -65,9 +70,9 @@ function blob_fixup() {
     esac
 }
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE" "$VENDOR" "$ROOT" false "$CLEAN_VENDOR"
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2019 The Paranoid Android Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The PixelExperience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,23 +28,22 @@ INITIAL_COPYRIGHT_YEAR=2019
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$ROOT"/vendor/lucid/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
 . "$HELPER"
 
-# Initialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$ROOT"
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT"
 
 # Copyright headers and guards
-write_headers "$DEVICE"
+write_headers
 
-# The standard device blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "$MY_DIR"/proprietary-files.txt
 
-# We are done!
+# Finish
 write_footers
