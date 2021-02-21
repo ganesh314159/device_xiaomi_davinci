8698c8dbe510a8fa7511510a2a06f1ff425345b0
trpsl <trpsl755@gmail.com>
2021-02-21 12:30:22 +0100

    davinci: update defconfig
    Change-Id: I2aaac4966e79c754673ac0e44c63276928f146e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := vendor/davinci-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/davinci
 
 # Platform
