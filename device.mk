7bfa0de20dfc89781712f19f6f19097b973ef60e
baalajimaestro <me@baalajimaestro.me>
2021-03-07 19:52:39 +0530

    davinci: Drop biometric dialog overlay
    This reverts commit bdf9116faf2a281baecbf5aa8f393dff5b8e092d.
    This reverts commit e93b436aa93268e893a7b26c367db2f3ce4062aa.
    This reverts commit fc1c4b4425c0036a08981ad2ec4eb337efd5d2ef.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- device.mk
+++ device.mk
@@ -40,12 +40,9 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-system
 
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
