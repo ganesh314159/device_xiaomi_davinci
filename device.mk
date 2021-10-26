492c1fe9bb53103f2f4e6f9043837f9199ce2255
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-26 09:57:13 -0400

    Move IFAA to another repo
    platform-bootclasspath cannot depend on something from a non-root
    soong namespace.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I23b571dd8e4f3e344748c76f320a61f2486e229c

--- device.mk
+++ device.mk
@@ -243,11 +243,7 @@ PRODUCT_HOST_PACKAGES += \
     signapk
 
 # IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+include vendor/xiaomi/sdm845-common-extra/ifaa.mk
 
 # Init
 PRODUCT_PACKAGES += \
