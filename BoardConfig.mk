0273bab903e3b9f9a53df4f54abb45938d806ffa
Arian <arian.kulmer@web.de>
2022-08-18 16:05:15 +0200

    davinci: Use LLVM binutils and LLVM integrated assembler
    Change-Id: I408c0e1b272af25e8e606e586a9e1d6e7082afba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_CONFIG := davinci_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    LLVM=1 \
+    LLVM_IAS=1
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
